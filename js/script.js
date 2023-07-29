35e51b37298b18fdd3db4679fc38b3e0a68b9c56
Carlos Ventura <carlosven37@gmail.com>
2023-07-28 18:09:03 -0600

    new advanced

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
-// JavaScript para mostrar/ocultar el menú de categorías al hacer clic en el ícono de hamburguesa
 const menuToggle = document.querySelector('.menu-toggle');
 const navMenu = document.getElementById('navMenu');
 const categoryMenu = document.querySelector('.category-menu');
 
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show');
-    categoryMenu.classList.toggle('show'); // Agregamos esta línea para mostrar/ocultar el menú de categorías
+    categoryMenu.classList.toggle('show'); 
 });
 
-// JavaScript para agregar el producto al carrito y mostrar la alerta
 const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
 
 addToCartButtons.forEach(button => {
     button.addEventListener('click', () => {
         const productName = button.previousElementSibling.previousElementSibling.textContent;
 
-        // Agregar clase 'show' para mostrar la alerta con una transición
         const alertBox = document.createElement('div');
         alertBox.classList.add('alert-box');
         alertBox.textContent = `¡${productName} ha sido agregado al carrito!`;
         document.body.appendChild(alertBox);
 
-        // Eliminar la alerta después de 3 segundos
         setTimeout(() => {
             document.body.removeChild(alertBox);
         }, 3000);
@@ -33,5 +29,5 @@ function toggleMenu() {
     const categoryMenu = document.getElementById('categoryMenu');
 
     navMenu.classList.toggle('show');
-    categoryMenu.classList.toggle('show'); // Agregamos esta línea para mostrar/ocultar el menú de categorías
+    categoryMenu.classList.toggle('show'); 
 }
