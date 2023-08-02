af9623c1fb4bf39a5b47fbacb0862b2bf447780d
Carlos Ventura <carlosven37@gmail.com>
2023-08-02 17:15:02 -0600

    ULTIMATE

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const categoryMenu = document.querySelector('.category-menu');
 
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show');
-    categoryMenu.classList.toggle('show'); 
+    categoryMenu.classList.toggle('show');
 });
 
 const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
@@ -29,5 +29,25 @@ function toggleMenu() {
     const categoryMenu = document.getElementById('categoryMenu');
 
     navMenu.classList.toggle('show');
-    categoryMenu.classList.toggle('show'); 
+    categoryMenu.classList.toggle('show');
+}
+
+// Agrega la función para realizar la búsqueda
+function searchProduct() {
+    const searchInput = document.querySelector('.search-bar input');
+    const searchTerm = searchInput.value.toLowerCase();
+
+    // Obtén todas las secciones de productos
+    const products = document.querySelectorAll('.product');
+
+    products.forEach(product => {
+        const productName = product.querySelector('h2').textContent.toLowerCase();
+
+        // Oculta los productos que no coinciden con el término de búsqueda
+        if (productName.includes(searchTerm)) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
 }
